869d5bff3cec962b42dfa70b0a0e44e656215242
Emily Cramer <emilycramer@Emilys-Air.hitronhub.home>
2017-02-18 17:55:49 -0800

    used a closure (named inner function) to extract a piece of logic specific to the one function. think this is a good alternative to placing comments above the code- worth doing for improvement in readability. used closure and then called it in render return statement to conditionally render one or another component depending on whether the weather component was fetching weather data

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -6,17 +6,23 @@ import OpenWeatherMap from 'OpenWeatherMap';
 
 var Weather = React.createClass({
   getInitialState: function(){
+    //you don't -need- to define all of the state variables from the outset.
+    //however, matt said it actually slows down the application is we don't so.. i will, but initialize tem withudnefined
     return {
-      city: "",
-      temp: ""
+      isLoading: false,
+      temp: undefined,
+      city: undefined
     }
   },
 
   handleNewCity: function(city){
+
+    this.setState({isLoading: true});
    //note: the referent of this always refers to whichever object executes th function
    //the success callback for a promise would be executed by the promise object, not this object
    //(no atomatic this binding in js)
-   //thus we use bind to create a new instance of handlenewtemp wherein this referent is bound to this component
+   //thus we use bind to create a new instance of handlenewtemp wherein this referent is bound to this componen
+
       OpenWeatherMap.getTemp(city).
       then(
         //note: you can also bind additional argument values to a functio with bind.
@@ -25,20 +31,35 @@ var Weather = React.createClass({
         this.handleNewTemp.bind(this, city),
         function(err){
             alert(err.message);
+            this.setState({isLoading: false});
         });
       //why must NEVER CALL FROM RENDER, without being contingent on an inout event or something
   },
 
    handleNewTemp: function(city, temp){
-      this.setState({city, temp}); //will trigger an automatic call to render.
+      this.setState({city, temp, isLoading: false}); //will trigger an automatic call to render.
     },
 
   render: function(){
+    let {city, temp, isLoading} = this.state;
+
+    //we can define a closure (function within a function) to encapsulate a piece of logic
+    //that is local to the function (avoid cluttering up the code with one off helper methods, but give them
+  //a name... better than comments!!!) clsures over comments. but beware, this binding is lost within closures
+  //and var (but not let) cannot be ccessed anymore
+    function renderMessage(){
+      if(isLoading){
+        return <h1>Fetching temperature data...</h1>;
+      } else if(city && temp){
+          return <WeatherMessage city={city} temp={temp}/>;
+      }
+    }
+
     return (
       <div>
         <h3>Get Weather</h3>
         <WeatherForm onNewCity={this.handleNewCity}/>
-        <WeatherMessage city={this.state.city} temp={this.state.temp}/>
+        {renderMessage()}
       </div>
       );
   }
--- public/bundle.js
+++ public/bundle.js
@@ -25012,32 +25012,61 @@
 	  displayName: 'Weather',
 
 	  getInitialState: function getInitialState() {
+	    //you don't -need- to define all of the state variables from the outset.
+	    //however, matt said it actually slows down the application is we don't so.. i will, but initialize tem withudnefined
 	    return {
-	      city: "",
-	      temp: ""
+	      isLoading: false,
+	      temp: undefined,
+	      city: undefined
 	    };
 	  },
 
 	  handleNewCity: function handleNewCity(city) {
+
+	    this.setState({ isLoading: true });
 	    //note: the referent of this always refers to whichever object executes th function
 	    //the success callback for a promise would be executed by the promise object, not this object
 	    //(no atomatic this binding in js)
-	    //thus we use bind to create a new instance of handlenewtemp wherein this referent is bound to this component
+	    //thus we use bind to create a new instance of handlenewtemp wherein this referent is bound to this componen
+
 	    _OpenWeatherMap2.default.getTemp(city).then(
 	    //note: you can also bind additional argument values to a functio with bind.
 	    //these arguments will be the first ONES in the function's list.
 	    //any other values that the caller passes in will come AFTERWARDS
 	    this.handleNewTemp.bind(this, city), function (err) {
 	      alert(err.message);
+	      this.setState({ isLoading: false });
 	    });
 	    //why must NEVER CALL FROM RENDER, without being contingent on an inout event or something
 	  },
 
 	  handleNewTemp: function handleNewTemp(city, temp) {
-	    this.setState({ city: city, temp: temp }); //will trigger an automatic call to render.
+	    this.setState({ city: city, temp: temp, isLoading: false }); //will trigger an automatic call to render.
 	  },
 
 	  render: function render() {
+	    var _state = this.state,
+	        city = _state.city,
+	        temp = _state.temp,
+	        isLoading = _state.isLoading;
+
+	    //we can define a closure (function within a function) to encapsulate a piece of logic
+	    //that is local to the function (avoid cluttering up the code with one off helper methods, but give them
+	    //a name... better than comments!!!) clsures over comments. but beware, this binding is lost within closures
+	    //and var (but not let) cannot be ccessed anymore
+
+	    function renderMessage() {
+	      if (isLoading) {
+	        return _react2.default.createElement(
+	          'h1',
+	          null,
+	          'Fetching temperature data...'
+	        );
+	      } else if (city && temp) {
+	        return _react2.default.createElement(_WeatherMessage2.default, { city: city, temp: temp });
+	      }
+	    }
+
 	    return _react2.default.createElement(
 	      'div',
 	      null,
@@ -25047,7 +25076,7 @@
 	        'Get Weather'
 	      ),
 	      _react2.default.createElement(WeatherForm, { onNewCity: this.handleNewCity }),
-	      _react2.default.createElement(_WeatherMessage2.default, { city: this.state.city, temp: this.state.temp })
+	      renderMessage()
 	    );
 	  }
 	});
