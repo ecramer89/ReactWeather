2fa701212bf9944d4233072d4eddd7de3660706f
Emily Cramer <emilycramer@Emilys-Air.hitronhub.home>
2017-02-19 09:37:57 -0800

    step 1... installing foundation and jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,28 @@
 /* config file for webpack allows us to pass in various middling tools to the webpack main transform command */
 //when we write a config file the webpack command on terminal (available because of global) will automatically apply the commands that we previously placed on command lin e(i.e., webpack entrypoint and endpoint)
 //the config file also enables us to specify more complicated dirctives, suchas the babelloader
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    //script! == modulename! (name of a special loader that we loaded with npm)
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  //externals: set of kvp pairs, where key is module name and value is name we want available in our modules
+  externals: {
+    jquery: 'jQuery'
+  },
+  //plugins allow us to make symbols from an external module available to our other scripts without needing to explicitly load in themodule each time (webpack will lookup the def in the required modules for us)
+  plugins: [
+    new webpack.ProvidePlugin({
+      //key: variable name to watch for
+      //value: module to replace it with
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
