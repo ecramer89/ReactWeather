bd5be5dcfe6f4be8646dffc1ea13c84c4fcf79d3
Emily Cramer <emilycramer@Emilys-Air.hitronhub.home>
2017-02-19 14:53:40 -0800

    uninstalled file loader

--- webpack.config.js
+++ webpack.config.js
@@ -40,8 +40,7 @@ module.exports = {
       WeatherMessage: "app/components/WeatherMessage.jsx",
       About: "app/components/About.jsx",
       Examples: "app/components/Examples.jsx",
-      OpenWeatherMap: "app/api/openWeatherMap.js",
-      kitten: "public/images/kitten.jpg"
+      OpenWeatherMap: "app/api/openWeatherMap.js"
     } //...sothe alias... needs to omit the first forward slash. not sure wh
   },
 
@@ -54,13 +53,6 @@ module.exports = {
       },
       test: /\.jsx?$/,
       exclude: /(node_modules|bower_components)/
-    },
-    {
-      loader: 'file-loader',
-      test: /(.jpg|.png)/,
-      options: {
-        name: '.public/images/[hash].[ext]'
-      }
     }
     ]
   },
